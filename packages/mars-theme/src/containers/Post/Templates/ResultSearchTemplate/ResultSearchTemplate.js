3cb8d8e31db8c2524068a41d86c0ca0e40d728cd
Sasha <sachazaets@ukr.net>
2020-08-18 14:11:04 +0300

    add teamplte

---
+++ packages/mars-theme/src/containers/Post/Templates/ResultSearchTemplate/ResultSearchTemplate.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {
+  Wrapper,
+} from './styles';
+
+const ResultSearchTemplate = () => (
+  <Wrapper>asdasdasd</Wrapper>
+);
+
+export default ResultSearchTemplate;
---
+++ packages/mars-theme/src/containers/Post/Templates/ResultSearchTemplate/styles.js
@@ -0,0 +1,5 @@
+import { styled } from 'frontity';
+
+export const Wrapper = styled.div`
+  padding: 25px 0;
+`;
