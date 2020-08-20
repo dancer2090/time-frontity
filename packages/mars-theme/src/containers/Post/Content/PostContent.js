745067e3adcd5ca7ff6e23700bb251c8761c8011
Sasha <sachazaets@ukr.net>
2020-08-20 14:38:46 +0300

    fixeds

--- packages/mars-theme/src/containers/Post/Content/PostContent.js
+++ packages/mars-theme/src/containers/Post/Content/PostContent.js
@@ -9,14 +9,14 @@ import PostPhotoTemplate from '../Templates/PostPhotoTemplate';
 
 const PostContent = ({ state, scrollRef = null }) => {
   const data = state.source.get(state.router.link);
-
+  console.log(data);
   return (
     <Switch>
       <MainTemplate scrollRef={scrollRef} when={state.router.link === '/' || state.router.link === '/uk/'} />
       <CategoryTemplate scrollRef={scrollRef} when={data.isCategory === true} />
+      <PostPhotoTemplate scrollRef={scrollRef} when={data.type === 'images'} />
       <PostTemplate scrollRef={scrollRef} when={data.isPostType} />
       <PhotoListTemplate scrollRef={scrollRef} when={data.isImagesArchive} />
-      <PostPhotoTemplate scrollRef={scrollRef} when={data.isImagesArchive} />
     </Switch>
   );
 };
