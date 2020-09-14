579467f6f25062f8fc0338fda9410495d7684713
yuriy-arttime <yuriy3304@gmail.com>
2020-09-14 10:43:46 +0300

    fix main title

--- frontity.settings.js
+++ frontity.settings.js
@@ -38,7 +38,7 @@ const settings = [
       "state": {
         "source": {
           "api": "https://time-admin.webbuilder.in.ua/wp-json",
-          "homepage": '/',
+          "homepage": '/main',
           "postsPage": '/blog',
           params: {
             per_page: 10,
